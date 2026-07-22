2b04701031e1b33c989894dbf265ed60f845a49c
lucassant <lucasfdesantana095@gmail.com>
2026-07-21 22:41:32 -0300

    delete: deletando arquivos

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <template>
   <div id="app" class="min-h-screen bg-gray-50">
-    <LoginScreen/>
-    <MainScreen/>
+    <router-view />
   </div>
 </template>
 
 <script>
-import MainScreen from './components/MainScreen.vue'
-import LoginScreen from './components/LoginScreen.vue'
-
 export default {
-  name: 'App',
-  components: {
-    MainScreen,
-    LoginScreen
-  }
+  name: 'App'
 }
 </script>
 
