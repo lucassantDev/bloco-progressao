8aef953109f6ac0a53a7b77d02e31bd9818bd34c
Lucas Santana <lucasfdesantana095@gmail.com>
2026-07-21 22:44:14 -0300

    refactor: refatorando arquivo index.js dentro da pasta routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginScreen from '../components/LoginScreen.vue'
 import MainScreen from '../components/MainScreen.vue'
-import NewUserScreen from '../components/NewUserScreen.vue'
+
 
 const routes = [
-  {
-    path: '/',
-    name: 'Login',
-    component: LoginScreen
-  },
-  {
-    path: '/NewUserScreen',
-    name: 'NewUser',
-    component: NewUserScreen
-  },
   {
     path: '/Main',
     name: 'Main',
