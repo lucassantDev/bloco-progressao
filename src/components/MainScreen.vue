7f4713901eef249a989f8b3fd84529a323fb14a2
lucassant <lucasfdesantana095@gmail.com>
2026-07-23 12:12:03 -0300

    feat: adicionando tempo de 25 minutos com botao de resetar

--- src/components/MainScreen.vue
+++ src/components/MainScreen.vue
@@ -12,18 +12,59 @@
                 </h1>
             </div>
 
-            <button class="p-2 rounded-md secondFontStyle-Button text-gray-200 trasition duration-100 hover:border-2 hover:border-gray-200">
-                começar
-            </button>
+            <div class='flex gap-6'>
+                <button
+                    @click="comecar"
+                    class="p-2 rounded-md secondFontStyle-Button text-gray-500 trasition duration-100 border-2 border-gray-500 hover:text-gray-200 hover:border-gray-200">
+                    começar
+                </button>
+                <button
+                    @click="resetar"
+                    class="p-2 rounded-md secondFontStyle-Button text-gray-500 trasition duration-100 border-2 border-gray-500 hover:text-gray-200 hover:border-gray-200">
+                    resetar
+                </button>
+            </div>
 
         </div>
         
     </div>
 </template>
 <script setup>
+import { ref, computed, onUnmounted } from 'vue'
 
-const time = new Date().toLocaleTimeString('pt-BR');
+const minutos = 25
+const segundosRestantes = ref(minutos * 60) // 1500 segundos
+const rodando = ref(false)
+let intervalo = null
 
+const time = computed(() => {
+  const min = Math.floor(segundosRestantes.value / 60).toString().padStart(2, '0')
+  const seg = (segundosRestantes.value % 60).toString().padStart(2, '0')
+  return `${min}:${seg}`
+})
+
+function comecar() {
+  if (rodando.value) return
+  rodando.value = true
+
+    // setInterval/setTimeOut -> arrow function, dentro da função, cria o bloco de código, depois passa o tempo que essa função vai aconter/ser executada
+  intervalo = setInterval(() => {
+    if (segundosRestantes.value > 0) {
+      segundosRestantes.value--
+    } else {
+      clearInterval(intervalo)
+      rodando.value = false
+    }
+  }, 1000)
+}
+
+function resetar(){
+    clearInterval(intervalo);
+    rodando.value = false;
+    segundosRestantes.value = minutos * 60 
+}
+
+onUnmounted(() => clearInterval(intervalo))
 
 </script>
 
